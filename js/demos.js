aa5fc25b915ef588e159157cd17473adb5fde8f4
musikpirat <drupal@christian-hufgard.de>
2013-12-14 15:35:42 +0100

    Datum hinzugefügt

--- js/demos.js
+++ js/demos.js
@@ -2,7 +2,7 @@
 var jahr = 2013;
 
 var demos = [
-{ "name":"Frankfurt" , "zeit":"15-19 Uhr", "ort":"Am Brunnen auf der Zeil", "link":"https://maps.google.de/maps?q=50.114555,8.683405&hl=de&ll=50.114555,8.683405&spn=0.000556,0.001635&sll=50.114555,8.683405&sspn=0.002222,0.006539&t=h&z=20"},
-{ "name":"Hamburg" , "zeit":"Offen", "ort":"Irgendwo beim Weihnachtsmarkt", "link":"" }
+{ "name":"Frankfurt" , "datum":"21.12.","zeit":"15-19 Uhr", "ort":"Am Brunnen auf der Zeil", "link":"https://maps.google.de/maps?q=50.114555,8.683405&hl=de&ll=50.114555,8.683405&spn=0.000556,0.001635&sll=50.114555,8.683405&sspn=0.002222,0.006539&t=h&z=20"},
+{ "name":"Hamburg" , "datum":"21.12.", "zeit":"Offen", "ort":"Irgendwo beim Weihnachtsmarkt", "link":"" }
 ];
 
